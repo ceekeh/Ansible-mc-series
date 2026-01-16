828d2bc6f0a7e7b2d54d1ed1e85b2e000e1460ff
ceekeh <142766320+ceekeh@users.noreply.github.com>
2026-01-15 20:35:06 -0400

    Update null-resource.tf

--- 01-Introduction/terraform files/null-resource.tf
+++ 01-Introduction/terraform files/null-resource.tf
@@ -1,13 +1,17 @@
 resource "null_resource" "vscode-config" {
   depends_on = [time_sleep.wait_for_instance]
 
+   
+      # the key we used for ssh conection is in the downloads folder of his macbook, bc ansible was downloaded in his macbk
+
 
   connection {
     type        = "ssh"
     host        = aws_instance.ubuntu.public_ip
     user        = "ubuntu"
     password    = ""
-    private_key = file("~/Downloads/ansible-key.pem")
+    private_key = file("~/Downloads/ansible-key.pem") #the key we used for ssh conection is in the downloads folder of his macbook, bc ansible was downloaded in his macbk
+    #but the illustration we did in terfrm, we created a key file & we used a path module to refer to the key file we used for ssh bc trfrm was instaled in the cloud ie ec2
   }
 
   provisioner "file" {
